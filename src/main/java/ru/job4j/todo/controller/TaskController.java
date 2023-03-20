5148cc344a7caba10e8376e2df1f9aed87daac74
SevaStopAll <Sevastopallrh@yandex.ru>
2023-03-20 22:12:21 +0400

    Now we have main page with the list of tasks.

--- src/main/java/ru/job4j/todo/controller/TaskController.java
+++ src/main/java/ru/job4j/todo/controller/TaskController.java
@@ -1,4 +1,81 @@
 package ru.job4j.todo.controller;
 
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.todo.model.Task;
+import ru.job4j.todo.service.TaskService;
+
+
+@Controller
+@RequestMapping("/tasks")
+@ThreadSafe
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public String getAll(Model model) {
+        model.addAttribute("tasks", taskService.findAll());
+        return "/tasks";
+    }
+
+    @GetMapping("/add")
+    public String getCreationPage(Model model) {
+        model.addAttribute("tasks", taskService.findAll());
+        return "tasks/add";
+    }
+
+    @PostMapping("/add")
+    public String create(@ModelAttribute Task task, Model model) {
+        try {
+            taskService.add(task);
+            return "redirect:/tasks";
+        } catch (Exception exception) {
+            model.addAttribute("message", exception.getMessage());
+            return "errors/404";
+        }
+    }
+
+    @GetMapping("/{id}")
+    public String getById(Model model, @PathVariable int id) {
+        var taskOptional = taskService.findById(id);
+        if (taskOptional.isEmpty()) {
+            model.addAttribute("message", "Вакансия с указанным идентификатором не найдена");
+            return "errors/404";
+        }
+        model.addAttribute("task", taskOptional.get());
+        return "tasks/one";
+    }
+
+    /*@PostMapping("/update")
+    public String update(@ModelAttribute Vacancy vacancy, @RequestParam MultipartFile file, Model model) {
+        try {
+            var isUpdated = vacancyService.update(vacancy, new FileDto(file.getOriginalFilename(), file.getBytes()));
+            if (!isUpdated) {
+                model.addAttribute("message", "Вакансия с указанным идентификатором не найдена");
+                return "errors/404";
+            }
+            return "redirect:/vacancies";
+        } catch (Exception exception) {
+            model.addAttribute("message", exception.getMessage());
+            return "errors/404";
+        }
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(Model model, @PathVariable int id) {
+        var isDeleted = vacancyService.deleteById(id);
+        if (!isDeleted) {
+            model.addAttribute("message", "Вакансия с указанным идентификатором не найдена");
+            return "errors/404";
+        }
+        return "redirect:/vacancies";
+    }*/
+
 }
--- src/main/java/ru/job4j/todo/store/TaskStore.java
+++ src/main/java/ru/job4j/todo/store/TaskStore.java
@@ -6,12 +6,14 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Task;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-@AllArgsConstructor
 public class TaskStore {
     private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
             .configure().build();
@@ -22,60 +24,83 @@ public class TaskStore {
         Session session = sf.openSession();
         try {
             session.beginTransaction();
+            session.save(task);
+            session.getTransaction().commit();
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
-        return null;
+        return task;
     }
 
-    public boolean replace(Task task) {
+    public boolean replace(int id, Task task) {
         Session session = sf.openSession();
+        boolean result = false;
         try {
             session.beginTransaction();
+            session.createQuery("UPDATE Task SET description =:tDescription WHERE id = :tId")
+                    .setParameter("tDescription", task.getDescription())
+                    .setParameter("tId", task.getId())
+                    .executeUpdate();
+            session.getTransaction().commit();
+            result = true;
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
-        return false;
+        return result;
     }
 
     public boolean delete(int id) {
         Session session = sf.openSession();
+        boolean result = false;
         try {
             session.beginTransaction();
+            session.createQuery(
+                            "DELETE Task WHERE id = :fId")
+                    .setParameter("fId", id)
+                    .executeUpdate();
+            session.getTransaction().commit();
+            result = true;
         } catch (Exception e) {
             session.getTransaction().rollback();
         }
-        return false;
+        return result;
     }
 
     public List<Task> findAll() {
         Session session = sf.openSession();
+        List result = new ArrayList<>();
         try {
             session.beginTransaction();
+            Query query = session.createQuery("from ru.job4j.todo.model.Task", Task.class);
+            result = query.getResultList();
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
-        return null;
+        return result;
     }
 
-    public List<Task> findByName() {
+    public Task findById(int id) {
         Session session = sf.openSession();
+        Task result = null;
         try {
             session.beginTransaction();
+            Query<Task> query = session.createQuery(
+                    "from Task as t where t.id = :fId", Task.class);
+            query.setParameter("fId", id);
+            result = query.uniqueResult();
+            session.getTransaction().commit();
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
-        return null;
-    }
-
-    public Task findById() {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
-        return null;
+        return result;
     }
 
     public void close() {
